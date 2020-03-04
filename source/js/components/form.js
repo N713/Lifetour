9bc9b21816049c65adc93bbc19fc47cb0e807ff7
ely <ely137137@gmail.com>
2020-03-05 00:06:58 +0300

    Баги

--- source/js/components/form.js
+++ source/js/components/form.js
@@ -1,10 +1,28 @@
 import IMask from "imask";
 
 const telInput = document.getElementById(`phone-number`);
+const nameInput = document.getElementById(`name`);
+const form = document.body.querySelector(`.questions .form`);
+const submitButton = form.querySelector(`.button`);
 
 const maskOptions = {
   mask: telInput.dataset.mask + '',
 };
 
 const mask = IMask(telInput, maskOptions);
+
+form.addEventListener(`submit`, (evt) => {
+  evt.preventDefault();
+
+  if(telInput.value.length !== telInput.dataset.mask.length){
+    telInput.parentElement.classList.add(`error`);
+  } else {
+    telInput.parentElement.classList.remove(`error`);
+    localStorage.setItem(`name`, nameInput.value);
+    localStorage.setItem(`phone`,telInput.value);
+    submitButton.setAttribute(`disabled`,``);
+  }
+});
+
 export {mask};
+
